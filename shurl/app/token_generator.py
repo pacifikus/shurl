eb8d8ec2b6c66f3e652419c033894534e9d770ad
pacificus <masterkristall@gmail.com>
2022-10-25 02:09:20 +0700

    HW5

--- shurl/app/token_generator.py
+++ shurl/app/token_generator.py
@@ -1,19 +1,18 @@
 import random
 import string
 
-from shurl.db import add_token_to_db
-from shurl.db import is_token_exists
-from shurl.exceptions import TokenAlreadyExistError
-
+from app.db import add_token_to_db
+from app.db import is_token_exists
+from app.exceptions import TokenAlreadyExistError
 
 symbols = string.ascii_letters + string.digits
 
 
-def generate(n=7):
+def generate(n: int = 7):
     return "".join(random.choices(symbols, k=n))
 
 
-def generate_token(url, custom_alias):
+def generate_token(url: str, custom_alias: str):
     if custom_alias:
         if is_token_exists(custom_alias):
             raise TokenAlreadyExistError(custom_alias)
